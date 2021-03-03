03b277c4c1a5c1029dec1f96aea7cb17c923eab6
P Paul Jonathan <ppauljonathan@gmail.com>
2021-03-03 11:27:53 +0530

    added redirect if file not in proper format

--- controllers/output.js
+++ controllers/output.js
@@ -5,6 +5,7 @@ const conversion=require('../public/js/converter')
 module.exports.getOutput=(req,res,next)=>{res.redirect('/');}
 
 module.exports.postOutput=(req,res,next)=>{
+    if(!req.file){return res.redirect('/');}
     const pathName=path.join(__dirname,'../','cache',req.file.filename);
     res.render('output',{
         title:'OUTPUT',
