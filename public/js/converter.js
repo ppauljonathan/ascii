fee3b1bf369ad61d04afad8f8dc1c482b28dd9c1
P Paul Jonathan <ppauljonathan@gmail.com>
2021-03-04 15:06:12 +0530

    added 8 color dithering(pls ignore the test code commented out)

--- public/js/converter.js
+++ public/js/converter.js
@@ -10,7 +10,7 @@ function conversion(){
     const p=document.querySelector(".output");
     let canv,c;
     let grey,str='';
-    const t=4;
+    const t=8;
     const img=new Image;
     img.src='cache/'+src;
     img.onload=function(){
@@ -27,15 +27,22 @@ function conversion(){
             {
                 grey=(d.data[i]*0.3+d.data[i+1]*0.59+d.data[i+2]*0.11);
                 grey=256/t*Math.floor(grey/(256/t));
-                d.data[i]=grey;
+                // d.data[i]=grey;
+                // d.data[i+1]=grey;
+                // d.data[i+2]=grey;
                 if(grey==0){str+='...';}
-                else if(grey==64){str+=';;;';}
-                else if(grey==128){str+='###';}
-                else{str+='&&&'}
+                else if(grey==32){str+=',,,';}
+                else if(grey==64){str+=':::';}
+                else if(grey==96){str+=';;;'}
+                else if(grey==128){str+='iii';}
+                else if(grey==160){str+='lll'}
+                else if(grey=="192"){str+='www'}
+                else if(grey=="224"){str+="WWW";}
             }
             if((i/4)%canv.width==canv.width-1){str+='\n';}
         }
-        p.style.fontSize=`${75/canv.width}vw`;
+        // c.putImageData(d,0,0);
+        p.style.fontSize=`${750/canv.width}px`;
         c.clearRect(0,0,canv.width,canv.height);
         p.innerHTML=str;
     }
